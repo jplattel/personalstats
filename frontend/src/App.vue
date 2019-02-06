d9f85428fcd1ea98487ed93b896d5519f360d131
Joost Plattel <jsplattel@gmail.com>
2019-02-06 22:52:23 +0100

    added blog to footer

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -47,7 +47,10 @@
     <!-- Footer -->
     <footer v-if="user" class="footer">
       <div class="container">
-        <span class="text-muted"><a href="https://workflowy.com/s/personalstats-releas/LkIWJ1VqMUnncyOE">Release log</a> - Questions or ideas, bugs or something else? <a href="mailto:jsplattel+personalstats@gmail.com">Email us!</a></span>
+        <span class="text-muted">
+          <a href="https://blog.personalstats.nl">Blog</a> | 
+          <a href="https://workflowy.com/s/personalstats-releas/LkIWJ1VqMUnncyOE">Release log</a> - Questions or ideas, bugs or something else? <a href="mailto:jsplattel+personalstats@gmail.com">Email us!</a>
+        </span>
       </div>
     </footer>
 
