0ed3a0708b97c2b120cff9941968bbe9546572ab
Joost Plattel <jsplattel@gmail.com>
2019-02-06 22:48:45 +0100

    fixed https in base url for blog

--- blog/deploy.sh
+++ blog/deploy.sh
@@ -1,6 +1,9 @@
 
-echo "Deploying a new version of the blog to blog.personalstats.nl..."
+echo "Building the blog"
+hugo
+
+echo "Deploying a new version to blog.personalstats.nl..."
 aws s3 sync public s3://blog.personalstats.nl --acl public-read --delete --profile joostplattel
 
-# echo "Invalidating cloudfront cache..."
-# aws cloudfront create-invalidation --distribution-id E3JJQ3OB31KR7J --paths "/*" --profile joostplattel
+echo "Invalidating cloudfront cache..."
+aws cloudfront create-invalidation --distribution-id E29JU26M9XN3IF --paths "/*" --profile joostplattel
